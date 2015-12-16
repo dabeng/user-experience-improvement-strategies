ec8aa801a63fd0b3c9722ac4a8e01411c4fed37e
dabeng <zhexiany@gmail.com>
2015-12-16 20:15:31 +0800

    append fixed note-bar

--- long-scrollbar/scripts.js
+++ long-scrollbar/scripts.js
@@ -1,1 +1,23 @@
-// Empty JS for your own code to be here
+
+'use strict';
+
+(function($){
+
+  function monitorScroll() {
+    var pageNote = $('.page-note');
+    var isDown = $(document).scrollTop() + $(window).height() - $('body')[0].scrollHeight + $('.page-footer').height() > 0 ? true : false;
+    var isAbove = pageNote.is('.above-page-footer');
+    if (isDown && !isAbove) {
+      $('#wrapper').css({ 'position': 'relative' });
+      pageNote.addClass('above-page-footer');
+    } else if (!isDown && isAbove) {
+      $('#wrapper').css({ 'position': '' });
+      pageNote.removeClass('above-page-footer');
+    }
+  }
+
+  $(function() {
+    $(document).on('scroll', monitorScroll);
+  });
+
+})(jQuery);
