6a8a4b17fbcf2f15885df63cf45c8091c376ea0e
dabeng <zhexiany@gmail.com>
2015-12-29 22:30:17 +0800

    append an example of js animation

--- preferred-css-animation/scripts.js
+++ preferred-css-animation/scripts.js
@@ -1,44 +1,12 @@
 
 'use strict';
 
+(function($){
 
-window.onload = function () {
-
-
-
-  function expandItems() {
-    if (collapseItemsId) {
-      cancelAnimationFrame(collapseItemsId);
-    }
-    var stop = true;
-    var answers = document.querySelectorAll('.js-answer');
-    for (var i = 0; i < answers.length; i++) {
-      if (answers[i].scrollHeight > answers[i].clientHeight) {
-        stop = false;
-      }
-    }
-    if (!stop) {
-      for (i = 0; i < answers.length; i++) {
-        if (answers[i].scrollHeight > answers[i].clientHeight) {
-          answers[i].previousElementSibling.classList.remove('collapse-mode');
-          answers[i].style.height = (answers[i].clientHeight + 10) + 'px';
-        }
-      }
-      expandItemsId = requestAnimationFrame(expandItems);
-    }
-  }
-
-  document.getElementById('btn-toggle').addEventListener('click', function() {
-    if (this.classList.contains('collapse-mode')) {
-      this.classList.remove('collapse-mode');
-      this.textContent = '▼ Unfold List';
-      expandItemsId = requestAnimationFrame(expandItems);
-    } else {
-      this.classList.add('collapse-mode');
-      this.textContent = '▶ Folded List';
-      collapseItemsId = requestAnimationFrame(collapseItems);
-    }
-  });
+  var collapseItemsId = null;
+  var expandItemsId = null;
+  var collapseItemId = null;
+  var expandItemId = null;
 
   function collapseItem(elem) {
     if (expandItemId) {
@@ -50,10 +18,34 @@ window.onload = function () {
     }
 
     if (!stop) {
-      elem.style.height = (elem.clientHeight - 10) + 'px';
+      elem.style.height = ((elem.clientHeight - 10) > 0 ? (elem.clientHeight - 10) : 0 ) + 'px';
       requestAnimationFrame(function() {
         collapseItemId = collapseItem(elem);
       });
+    }
+  }
+
+  function collapseItems() {
+    if (expandItemsId) {
+      cancelAnimationFrame(expandItemsId);
+    }
+    var stop = true;
+    var answers = document.querySelectorAll('.para');
+    for (var i = 0; i < answers.length; i++) {
+      if (answers[i].clientHeight > 0) {
+        stop = false;
+      }
+    }
+    if (!stop) {
+      for (i = 0; i < answers.length; i++) {
+        answers[i].previousElementSibling.classList.add('collapse-mode');
+        if ((answers[i].clientHeight - 10) < 10) {
+          answers[i].style.height = '0px';
+        } else {
+          answers[i].style.height = (answers[i].clientHeight - 10) + 'px';
+        }
+      }
+      collapseItemsId = requestAnimationFrame(collapseItems);
     }
   }
 
@@ -76,60 +68,61 @@ window.onload = function () {
     }
   }
 
-  var questions = document.querySelectorAll('.js-question');
-  for (var i = 0; i < questions.length; i++) {
-    questions[i].addEventListener('click', function(event) {
-      var question = event.target;
-      if (question.classList.contains('collapse-mode')) {
-        question.classList.remove('collapse-mode');
-        requestAnimationFrame(function() {
-          expandItemId = expandItem(question.nextElementSibling);
-        });
-      } else {
-        question.classList.add('collapse-mode');
-        requestAnimationFrame(function() {
-          collapseItemId = collapseItem(question.nextElementSibling);
-        });
-      }
-    });
-  }
-
-};
-
-
-(function($){
-
-  var collapseItemsId = null;
-  var expandItemsId = null;
-  var collapseItemId = null;
-  var expandItemId = null;
-
-  function collapseItems() {
-    if (expandItemsId) {
-      cancelAnimationFrame(expandItemsId);
+  function expandItems() {
+    if (collapseItemsId) {
+      cancelAnimationFrame(collapseItemsId);
     }
     var stop = true;
-    var answers = document.querySelectorAll('.js-answer');
+    var answers = document.querySelectorAll('.para');
     for (var i = 0; i < answers.length; i++) {
-      if (answers[i].clientHeight > 0) {
+      if (answers[i].scrollHeight > answers[i].clientHeight) {
         stop = false;
       }
     }
     if (!stop) {
       for (i = 0; i < answers.length; i++) {
-        answers[i].previousElementSibling.classList.add('collapse-mode');
-        if ((answers[i].clientHeight - 10) < 10) {
-          answers[i].style.height = '0px';
-        } else {
-          answers[i].style.height = (answers[i].clientHeight - 10) + 'px';
+        if (answers[i].scrollHeight > answers[i].clientHeight) {
+          answers[i].previousElementSibling.classList.remove('collapse-mode');
+          answers[i].style.height = (answers[i].clientHeight + 10) + 'px';
         }
       }
-      collapseItemsId = requestAnimationFrame(collapseItems);
+      expandItemsId = requestAnimationFrame(expandItems);
     }
   }
 
   $(function() {
-    $(document).on('scroll', monitorScroll);
+
+    $('#btn-toggle').on('click', function() {
+      if (this.classList.contains('collapse-mode')) {
+        this.classList.remove('collapse-mode');
+        this.childNodes[0].classList.remove('fa-caret-right');
+        this.childNodes[0].classList.add('fa-caret-down');
+        this.childNodes[1].textContent = ' collapse artical';
+        expandItemsId = requestAnimationFrame(expandItems);
+      } else {
+        this.classList.add('collapse-mode');
+        this.childNodes[0].classList.remove('fa-caret-down');
+        this.childNodes[0].classList.add('fa-caret-right');
+        this.childNodes[1].textContent = ' expand article';
+        collapseItemsId = requestAnimationFrame(collapseItems);
+      }
+    });
+
+    $('.subhead').on('click', function(event) {
+      var subhead = event.target;
+      if (subhead.classList.contains('collapse-mode')) {
+        subhead.classList.remove('collapse-mode');
+        requestAnimationFrame(function() {
+          expandItemId = expandItem(subhead.nextElementSibling);
+        });
+      } else {
+        subhead.classList.add('collapse-mode');
+        requestAnimationFrame(function() {
+          collapseItemId = collapseItem(subhead.nextElementSibling);
+        });
+      }
+    });
+
   });
 
 })(jQuery);
