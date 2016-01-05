d2703337496ccd0e0e2da21250fa6057949635b8
dabeng <zhexiany@gmail.com>
2016-01-05 21:59:59 +0800

    Mid: apend tabs example

---
+++ usability/improve-tabs/scripts.js
@@ -0,0 +1,17 @@
+
+'use strict';
+
+(function($){
+
+
+
+  $(function() {
+
+$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+  // e.target // newly activated tab
+  // e.relatedTarget // previous active tab
+})
+
+  });
+
+})(jQuery);
