4d5609dcf4b0d0b94000047deae34f0ec9026c87
dabeng <zhexiany@gmail.com>
2016-01-07 21:08:31 +0800

    Mid: animate tabs

--- usability/improve-tabs/scripts.js
+++ usability/improve-tabs/scripts.js
@@ -9,7 +9,7 @@
 
 $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
   // e.target // newly activated tab
-  // e.relatedTarget // previous active tab
+  e.relatedTarget.style['border-bottom-color']='#fff'; // previous active tab
 })
 
   });
