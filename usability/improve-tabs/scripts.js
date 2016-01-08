1592e546db995b5bbfb1a5e95bebe67d524d6aa9
dabeng <zhexiany@gmail.com>
2016-01-08 11:55:03 +0800

    Animated tabs

--- usability/improve-tabs/scripts.js
+++ usability/improve-tabs/scripts.js
@@ -9,7 +9,17 @@
 
 $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
   // e.target // newly activated tab
-  e.relatedTarget.style['border-bottom-color']='#fff'; // previous active tab
+  // e.relatedTarget.style['border-bottom-color']='#fff'; // 
+  var preTab = e.relatedTarget;// previous active tab
+    // -> removing the class
+  preTab.classList.remove('change-border-color');
+  
+  // -> triggering reflow /* The actual magic */
+  // without this it wouldn't work. Try uncommenting the line and the transition won't be retriggered.
+  preTab.style.offsetWidth = preTab.offsetWidth;
+  
+  // -> and re-adding the class
+  preTab.classList.add('change-border-color');
 })
 
   });
