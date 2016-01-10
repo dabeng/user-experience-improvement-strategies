d525750277c0ee02c4e00afef9a62d2707ea4583
dabeng <zhexiany@gmail.com>
2016-01-10 10:38:44 +0800

    append example of animated pills

---
+++ animation/animated-pills/scripts.js
@@ -0,0 +1,25 @@
+
+'use strict';
+
+(function($){
+
+  $(function() {
+
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+      // e.target // newly activated tab
+      // e.relatedTarget.style['border-bottom-color']='#fff'; // 
+      var preTab = e.relatedTarget;// previous active tab
+      // removing the class
+      preTab.classList.remove('change-border-color');
+  
+      // -> triggering reflow /* The actual magic */
+      // without this it wouldn't work. Try uncommenting the line and the transition won't be retriggered.
+      preTab.style.offsetWidth = preTab.offsetWidth;
+  
+      // re-adding the class
+      preTab.classList.add('change-border-color');
+    });
+
+  });
+
+})(jQuery);
