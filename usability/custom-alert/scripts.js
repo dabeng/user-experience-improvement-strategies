a20e05fbed3b22cd32c71cb718e5e084a08d5a63
dabeng <zhexiany@gmail.com>
2016-01-17 21:17:13 +0800

    Mid: alert

--- usability/custom-alert/scripts.js
+++ usability/custom-alert/scripts.js
@@ -5,14 +5,16 @@
 
   $(function() {
 
-    $('#basic-alert').on('click', function (event) {
-      $('#alert-overlay').addClass('show-alert');
+    $('.btn').on('click', function (event) {
+      $('#alert-overlay').addClass('show-alert')
+        .find('.custom-alert').offset($(event.target).offset());
 
     });
 
     $('.custom-alert').on('click', '.btn-cancel', function(event) {
       
       $('#alert-overlay').removeClass('show-alert');
+      $(document).scrollTop();
     });
 
 
