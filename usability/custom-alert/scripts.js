e35e858e2400ad0cfa736899876e6df9a111e8e0
dabeng <zhexiany@gmail.com>
2016-01-20 09:18:55 +0800

    Fix-bug: append the redundant overlay of alert box

--- usability/custom-alert/scripts.js
+++ usability/custom-alert/scripts.js
@@ -8,31 +8,37 @@
     if (type === 0) {
 
     } else if (type === 1) {
-      $('body').append(
-        '<div id="alert-overlay">' +
-          '<div class="custom-alert">' +
-            '<span class="alert-icon">?</span>' +
-            '<p class="alert-message"></p>' +
-            '<div class="alert-buttons">' +
-              '<button type="button" class="btn btn-success btn-ok">OK</button>' +
-              '<button type="button" class="btn btn-default btn-cancel">Cancel</button>' +
+      if (!$('#alert-overlay').length) {
+        $('body').append(
+          '<div id="alert-overlay">' +
+            '<div class="custom-alert">' +
+              '<span class="alert-icon">?</span>' +
+              '<p class="alert-message"></p>' +
+              '<div class="alert-buttons">' +
+                '<button type="button" class="btn btn-success btn-ok">OK</button>' +
+                '<button type="button" class="btn btn-default btn-cancel">Cancel</button>' +
+              '</div>' +
             '</div>' +
-          '</div>' +
-        '</div>')
-        .find('#alert-overlay').find('.alert-message').text(message)
-        .end().find('.btn-ok').on('click',function() {
-          $('#alert-overlay').removeClass('show-alert');
-          dtd.resolve(true);
-        })
-        .siblings('.btn-cancel').on('click',function() {
-          $('#alert-overlay').removeClass('show-alert');
-          dtd.resolve(false);
-        });
-        // With the help of the following line, we enfore browser to repaint in order to
-        // fire up the first time transition of alert icon.
-        var overlay = $('#alert-overlay')[0];
-        overlay.style.offsetWidth = overlay.offsetWidth;
-        overlay.classList.add('show-alert');
+          '</div>')
+          .find('#alert-overlay').find('.alert-message').text(message)
+          .siblings('.alert-buttons').children().data('dtd', dtd)
+          .filter('.btn-ok').on('click', function() {
+            $('#alert-overlay').removeClass('show-alert');
+            $(this).data('dtd').resolve(true);
+          })
+          .siblings('.btn-cancel').on('click', function() {
+            $('#alert-overlay').removeClass('show-alert');
+            $(this).data('dtd').resolve(false);
+          });
+          // With the help of the following line, we enfore browser to repaint in order to
+          // fire up the first time transition of alert icon.
+          var overlay = $('#alert-overlay')[0];
+          overlay.style.offsetWidth = overlay.offsetWidth;
+          overlay.classList.add('show-alert');
+      } else {
+        $('#alert-overlay').addClass('show-alert')
+          .find('.alert-buttons').children().data('dtd', dtd);
+      }
     }
 
     return dtd;
